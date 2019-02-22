5ded05a39e7b2c3c140bc800f4e106e17498f70e
DreamLi2015 <46148958+DreamLi2015@users.noreply.github.com>
2019-02-22 14:54:09 +0800

    Update MGAppTest.java

--- src/test/java/com/baizhi/mybatisgenerator/MGAppTest.java
+++ src/test/java/com/baizhi/mybatisgenerator/MGAppTest.java
@@ -97,6 +97,6 @@ public class MGAppTest {
     @Test
     public void test2() {
         System.out.println("test");
-        System.out.println("test2");
+        System.out.println("test3");
     }
 }
